f7e392d459ff83a18cf5d84c48bc8dc8c4cd913d
gi3-14 <g.o.idehen@gmail.com>
2022-04-09 04:38:57 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,32 @@
-#include <stdio.h>
-/**
- *main - entry point 
- *
- *Return: 0 after printing the function
+/*
+
+ * File: 4-puts.c
+
+ * Auth: Idehen osariemen
+
  */
+
+#include <stdio.h>
+
+/**
+
+ * main - Prints "Programming is like building a multilingual
+
+ *                puzzle, followed by a new line.
+
+ *
+
+ * Return: Always 0.
+
+ */
+
 int main(void)
+
 {
-puts("\"Programming is like building a multilingual puzzle"); 
-return (0);
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+
+	return (0);
+
 }
