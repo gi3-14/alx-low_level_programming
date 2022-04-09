161de92b2bb8f96ad77cf22b78a38b44d505496f
gi3-14 <g.o.idehen@gmail.com>
2022-04-09 04:34:02 -0700

    hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,16 @@
-#include <stdio.h> 
-/**
- *main - Entry point
- *My hello, printf task
- *Return: return 1 after program execution
+/*
+ * File: 5-printf.c
+ * Auth: Idehen osariemen
  */
-int main (void)
+#include <stdio.h>
+/**
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, \n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
